154014bcaf132582c587708b9a7353ccfd4713af
onursercanyilmaz <onursercanyilmaz@gmail.com>
2023-03-05 12:31:31 +0300

    body parser added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const PORT = process.env.PORT || 8000;
 var express = require("express");
-var bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
+var bodyParser = require("body-parser");
 require("dotenv").config({ path: ".env" });
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+const app = express();
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: "true" }));
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -16,24 +19,36 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 console.log(process.env.OPENAI_API_KEY);
 
-app.get(`${process.env.API_LINK}`, (req, res) => {
-  openai
-    .createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: req.body.question }],
-    })
-    .then((response) => {
-      if (response.data.length == 0) {
-        res.status(200).send([
-          {
-            status: 200,
-            error: "No data found",
-          },
-        ]);
-      } else {
-        res.status(200).send(response.data);
-      }
-    });
+app.post(`${process.env.API_LINK}`, async (req, res) => {
+  const question = req.body.question;
+  console.log("question: ", question);
+
+  try {
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: question }],
+      })
+      .then((response) => {
+        if (response.data.length == 0) {
+          res.status(200).send([
+            {
+              status: 200,
+              error: "No data found",
+            },
+          ]);
+        } else {
+          res.status(200).send(response.data);
+        }
+      });
+  } catch (err) {
+    res.send([
+      {
+        status: err.status,
+        error: err.messages,
+      },
+    ]);
+  }
 });
 
 app.listen(process.env.PORT || 8000, () =>
